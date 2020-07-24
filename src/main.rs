116b63648b9cde86141d772d9fc599e1becb5f21
Martin Zackrisson <martin.zackrisson@fripost.org>
2020-07-24 12:02:44 +0200

    better names

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::convert::TryInto;
 use std::fmt::Debug;
 
 #[derive(Debug)]
-struct Shape {pub size_x: usize, pub size_y: usize}
+pub struct Point {pub x: usize, pub y: usize}
 
 struct World {
     zones: Vec<Vec<u16>>,
@@ -11,21 +11,42 @@ struct World {
 }
 
 trait Board {
-    fn shape(&self) -> Shape;
+    fn shape(&self) -> Point;
     fn new_with<T: Clone>(&self, value: T) -> Vec<Vec<T>>;
+    fn fill(&mut self, x0: usize, x1: usize, y0: usize, y1: usize, when: u16, value: u16);
+    fn mark_rnd_position(&mut self, value: u16) -> Point; 
 }
 
 impl Board for Vec<Vec<u16>> {
-    fn shape(&self) -> Shape {
-        Shape {
-            size_x: self[0].len(),
-            size_y: self.len(),
+    fn shape(&self) -> Point {
+        Point {
+            x: self[0].len(),
+            y: self.len(),
         }
     }
     
     fn new_with<U: Clone>(&self, value: U) -> Vec<Vec<U>> {
        let s = self.shape();
-       vec![vec![value; s.size_x]; s.size_y]
+       vec![vec![value; s.x]; s.y]
+    }
+
+    fn fill(&mut self, x0: usize, x1: usize, y0: usize, y1: usize, when: u16, value: u16) {
+        for x in x0..x1 {
+            for y in y0..y1 {
+                if self[y][x] == when {
+                    self[y][x] = value;
+                }
+            }
+        }
+    }
+
+    fn mark_rnd_position(&mut self, value: u16) -> Point {
+        let s = self.shape();
+        let mut rng = rand::thread_rng();
+        let source_x = rng.gen_range(0, s.x);
+        let source_y = rng.gen_range(0, s.y);
+        self[source_y][source_x] = value;
+        Point{x: source_x, y: source_y}
     }
 }
 
@@ -44,51 +65,34 @@ fn add_zones_rects(board: &mut Vec<Vec<u16>>, zones: u16) {
     if zones < 2 {
         return;
     }
-    let size_y = board.len();
-    let size_x = board[0].len();
-    let mut rng = rand::thread_rng();
-    let source_x = rng.gen_range(0, size_x);
-    let source_y = rng.gen_range(0, size_y);
-    board[source_y][source_x] = 1;
-    let areas = get_zone_sizes(zones, size_x, size_y, 0.75);
-        
-    let mut x0 = source_x;
-    let mut x1 = source_x + 1;
-    let mut y0 = source_y;
-    let mut y1: usize = source_y + 1;
+    let shape = board.shape();
+    let areas = get_zone_sizes(zones, shape.x, shape.y, 0.75);
+    let source = board.mark_rnd_position(1);
+    let mut x0 = source.x;
+    let mut x1 = source.x + 1;
+    let mut y0 = source.y;
+    let mut y1: usize = source.y + 1;
 
+    let mut rng = rand::thread_rng();
     for zone in 1..zones {
         while areas[zone as usize] > (((x1 - x0) * (y1 - y0)) as u16) {
             let grow = rng.gen_range(0, 4);
             match grow {            
                 0 => if x0 > 1 {x0 -= 1},
-                1 => x1 = (x1 + 1).min(size_x),
+                1 => x1 = (x1 + 1).min(shape.x),
                 2 => if y0 > 1 {y0 -= 1},
-                3 => y1 = (y1 + 1).min(size_y),
+                3 => y1 = (y1 + 1).min(shape.y),
                 _ => println!("{}", grow),
             };
-        }
-        for x in x0..x1 {
-            for y in y0..y1 {
-                if board[y][x] == 0 {
-                    board[y][x] = zone + 1;
-                }
-            }
+            board.fill(x0, x1, y0, y1, 0, zone + 1);
         }
     }
-
-    for x in 0..size_x{
-        for y in 0..size_y{
-            if board[y][x] == 0 {
-                board[y][x] = zones + 1;
-            }
-        }
-    }
+    board.fill(0, shape.x, 0, shape.y, 0, zones + 1);
 }
 
 fn main() {
-    let shape = Shape{  size_x: 4 * 16, size_y: 16};
-    let zones = vec![vec![0; shape.size_x]; shape.size_y];
+    let shape = Point{ x: 4 * 16, y: 16};
+    let zones = vec![vec![0; shape.x]; shape.y];
     let fog_curve = zones.new_with(0);
     let mut world = World {
         zones,
